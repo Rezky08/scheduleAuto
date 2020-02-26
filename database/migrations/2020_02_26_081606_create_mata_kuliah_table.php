fca43f2eeaa46fc1c82cd0c92821875125328469
rezky setiawan <rezky.setiawan85@gmail.com>
2020-02-26 15:19:58 +0700

    add mata kuliah migration

---
+++ database/migrations/2020_02_26_081606_create_mata_kuliah_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMataKuliahTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kode_matkul', 10);
+            $table->integer('matkul_sks')->unsigned()->nullable()->default(5);
+            $table->boolean('matkul_status')->nullable()->default(true);
+            $table->string('kode_prodi', 10)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+}
