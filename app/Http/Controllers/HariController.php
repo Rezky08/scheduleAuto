4eab75687f1d93bb811c346cbbc3ef247df48bec
ravimelann123 <melan485@gmail.com>
2020-03-05 10:59:54 +0700

    fitur insert hari selesai

--- app/Http/Controllers/HariController.php
+++ app/Http/Controllers/HariController.php
@@ -13,9 +13,27 @@ class HariController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // check apa ada parameter id
+        if ($request->id) {
+            return $this->show($request);
+        }
+
+        try {
+            $hari = hari::all();
+            $response = [
+                'status' => 200,
+                'data' => $hari
+            ];
+            return response()->json($response, 200);
+        } catch (\Throwable $e) {
+            $response = [
+                'status' => 500,
+                'message' => $e
+            ];
+            return response()->json($response, 500);
+        }
     }
 
     /**
@@ -26,7 +44,39 @@ class HariController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $rules = [
+            'nama_hari' => ['required'],
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $response = [
+                'status' => 400,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+        $insertToDB = [
+            'nama_hari' => $request->nama_hari,
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+        try {
+            hari::insert($insertToDB);
+        } catch (\Throwable $e) {
+            $response = [
+                'status' => 500,
+                'message' => $e
+            ];
+            return response()->json($response, 500);
+        }
+
+        $response = [
+            'status' => 200,
+            'message' => 'hari kuliah ' . $request->nama_hari . ' Berhasil ditambahkan'
+        ];
+        return response()->json($response, 200);
     }
 
     /**
@@ -35,9 +85,29 @@ class HariController extends Controller
      * @param  \App\Hari  $hari
      * @return \Illuminate\Http\Response
      */
-    public function show(Hari $hari)
+    public function show(Request $request)
     {
-        //
+        $rules = [
+            'id' => ['required', 'exists:hari,id']
+        ];
+        $message = [
+            'id.exists' => 'sorry, we cannot find what are you looking for.'
+        ];
+        $validator = Validator::make($request->all(), $rules, $message);
+        if ($validator->fails()) {
+            $response = [
+                'status' => 400,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+        $hari = hari::where('id', $request->id)->first();
+        $response = [
+            'status' => 200,
+            'data' => $hari
+        ];
+        return response()->json($response, 200);
     }
 
     /**
