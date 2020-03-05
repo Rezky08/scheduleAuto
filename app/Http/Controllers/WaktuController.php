8cede872f871519eb356e76fe1d2a81514b21329
ravimelann123 <melan485@gmail.com>
2020-03-05 14:05:11 +0700

    create controller api model waktu

---
+++ app/Http/Controllers/WaktuController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Waktu;
+use Illuminate\Http\Request;
+
+class WaktuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Waktu  $waktu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Waktu $waktu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Waktu  $waktu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Waktu $waktu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Waktu  $waktu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Waktu $waktu)
+    {
+        //
+    }
+}
