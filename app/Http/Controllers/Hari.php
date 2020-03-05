ef50429bffe2103ed4781742d54820364c5ddaa0
ravimelann123 <melan485@gmail.com>
2020-03-05 10:44:17 +0700

    pembuatan model dan controller Hari

---
+++ app/Http/Controllers/Hari.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Hari;
+use Illuminate\Http\Request;
+
+class Hari extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Hari  $hari
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hari $hari)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Hari  $hari
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hari $hari)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Hari  $hari
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hari $hari)
+    {
+        //
+    }
+}
