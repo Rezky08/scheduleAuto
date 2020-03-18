a0495ea1cc3673cc168321cfa563906d0e615310
ravimelann123 <melan485@gmail.com>
2020-03-18 17:41:04 +0700

    penambahan validasi error

--- resources/views/program_studi.blade.php
+++ resources/views/program_studi.blade.php
@@ -16,6 +16,28 @@
 
         <div id="content" class="p-4 p-md-5 pt-5">
             <br />
+
+            @if ($errors->any())
+            @foreach ($errors->all() as $error)
+            <div class="alert alert-danger alert-dissmisable fade show" role="alert">
+                <strong>{{$error}}</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endforeach
+            @endif
+            @if (session('success'))
+            @foreach (session('success') as $success)
+            <div class="alert alert-success alert-dissmisable fade show" role="alert">
+                <strong>{{$success}}</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endforeach
+            @endif
+
             <!-- Button trigger modal -->
             <div class="card">
                 <div class="card-header">
