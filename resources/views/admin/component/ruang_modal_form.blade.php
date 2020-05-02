a49061133b297a89e120aa155e31268ed0d48b23
ravimelann123 <melan485@gmail.com>
2020-05-02 20:27:16 +0700

    CRUD RUANG DONE

---
+++ resources/views/admin/component/ruang_modal_form.blade.php
@@ -0,0 +1,54 @@
+@component('admin.component.modal_form')
+
+@slot('modal_id')
+{{$modal_id}}
+@endslot
+
+@isset($modal_header_class)
+@slot('modal_header_class')
+{{$modal_header_class}}
+@endslot
+@endisset
+
+@isset($modal_title_class)
+@slot('modal_title_class')
+{{$modal_title_class}}
+@endslot
+@endisset
+
+@slot('action')
+{{$action}}
+@endslot
+
+@slot('form_fields')
+<div class="form-group">
+    <label>nama_ruang</label>
+    <input type="text" name="nama_ruang" class="form-control border border-secondary" placeholder="nama_ruang">
+
+    @if($errors->has('nama_ruang'))
+    <div class="text-danger">
+        {{ $errors->first('nama_ruang')}}
+    </div>
+    @endif
+
+</div>
+
+<div class="form-group">
+    <label>keterangan</label>
+    <input type="text" name="keterangan" class="form-control border border-secondary" placeholder="keterangan">
+
+    @if($errors->has('keterangan'))
+    <div class="text-danger">
+        {{ $errors->first('keterangan')}}
+    </div>
+    @endif
+
+</div>
+@endslot
+
+@slot('modal_footer')
+<div class="form-group">
+    <input type="submit" class="btn btn-primary" value="Simpan">
+</div>
+@endslot
+@endcomponent
---
+++ resources/views/admin/modal/modal_ruang.blade.php
@@ -0,0 +1,111 @@
+@section('MIRUANG')
+@component('admin.component.ruang_modal_form')
+@slot('modal_id')
+modaliruang
+@endslot
+
+@slot('modal_title')
+Tambah ruang
+@endslot
+
+@slot('action')
+/ruang/add
+@endslot
+@endcomponent
+@endsection
+
+@section('MURUANG')
+@component('admin.component.ruang_modal_form')
+@slot('modal_id')
+modaluruang
+@endslot
+
+@slot('modal_title')
+Ubah Mata Kuliah
+@endslot
+
+@slot('action')
+/ruang/update/
+@endslot
+@endcomponent
+@endsection
+
+@section('MDRUANG')
+@component('admin.component.modal_form')
+@slot('modal_id')
+modaldruang
+@endslot
+
+@slot('modal_title')
+<h5 class="text-white">Hapus ruang</h5>
+@endslot
+
+@slot('action')
+/ruang/delete/
+@endslot
+
+@slot('modal_header_class')
+bg-danger
+@endslot
+
+@slot('form_fields')
+<strong id="modal-message">Apakah anda ingin menghapus ruang ini ?</strong>
+@endslot
+
+@slot('modal_footer')
+<button type="submit" class="btn btn-warning">Ya</button>
+<button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">
+    Tidak
+</button>
+@endslot
+
+@endcomponent
+@endsection
+
+@section('MODALJSRUANG')
+<script>
+    let ruang = @json($ruang);
+
+// Button update ruang
+$('button[data-target="#modaluruang"]').on('click', function () {
+    row_parent = $(this).parents('tr');
+    id_ruang = $(row_parent).attr('id');
+    ruang_item = ruang.find(function (item,index) {
+        if(item.id==id_ruang){
+            return item;
+        }
+     });
+
+    form = $("#modaluruang #formmodal");
+    $(form).find("[name='nama_ruang']").val(ruang_item.nama_ruang);
+    $(form).find("[name='keterangan']").val(ruang_item.keterangan);
+
+    act = '/ruang/update/';
+    act = act+id_ruang;
+    $(form).attr('action',act);
+});
+// Button delete ruang
+$("button[data-target='#modaldruang']").on('click', function () {
+    row_parent = $(this).parents('tr');
+    id_ruang = $(row_parent).attr('id');
+    nama_ruang = $(row_parent).find("#nama_ruang").text()
+    message = "Apakah anda ingin menghapus ruang  "+nama_ruang+" ("+id_ruang+") ?";
+    $("#modaldruang").find('#modal-message').text(message);
+    // act = $("#modaldruang").find("form").attr('action');
+    act = '/ruang/delete/';
+    act = act+id_ruang;
+    $("#modaldruang").find("form").attr('action',act);
+});
+</script>
+@endsection
+
+@section('MODAL')
+<!-- MODAL INPUT -->
+@yield('MIRUANG')
+<!-- MODAL UPDATE -->
+@yield('MURUANG')
+<!-- MODAL DELETE -->
+@yield('MDRUANG')
+<!-- MODAL JS -->
+@yield('MODALJSRUANG')
+@endsection
